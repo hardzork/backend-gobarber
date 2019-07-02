82ef5e6faf71cd0ca3b714103117dbf76f740dcd
Robinson Junior <hardzork@macjr.local>
2019-07-02 17:34:04 -0300

    config multer and add appointments

--- src/routes.js
+++ src/routes.js
@@ -1,23 +1,28 @@
 import { Router } from 'express';
+import multer from 'multer';
+import multerConfig from './config/multer';
 
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
+import FileController from './app/controllers/FileController';
+import ProviderController from './app/controllers/ProviderController';
+import AppointmentController from './app/controllers/AppointmentController';
 
 import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
 routes.post('/users', UserController.store);
-routes.put('/users', authMiddleware, UserController.update);
-
 routes.post('/sessions', SessionController.store);
 
-// routes.get('/', async (req, res) => {
-//   const user = await User.create({
-//     name: 'Diego Fernandes',
-//     email: 'diego@rocketseat.com.br',
-//     password_hash: '123',
-//   });
-//   return res.json(user);
-// });
+routes.use(authMiddleware);
+
+routes.put('/users', UserController.update);
+routes.get('/providers', ProviderController.index);
+routes.get('/providers', ProviderController.index);
+routes.get('/appointments', AppointmentController.index);
+routes.post('/appointments', AppointmentController.store);
+routes.post('/files', upload.single('file'), FileController.store);
+
 export default routes;

4ba2d2c9bc09068fcb1d107fadd6d59d4758bb72
Robinson Junior <hardzork@macjr.local>
2019-07-08 14:09:13 -0300

    environment variables

--- src/config/mail.js
+++ src/config/mail.js
@@ -1,10 +1,10 @@
 export default {
-  host: 'smtp.mailtrap.io',
-  port: 2525,
+  host: process.env.MAIL_HOST,
+  port: process.env.MAIL_PORT,
   secure: false,
   auth: {
-    user: 'f441d2bb249d27',
-    pass: '6180c56e9ef0f5',
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS,
   },
   default: {
     from: 'Equipe GoBarber <noreply@gobarber.com>',
